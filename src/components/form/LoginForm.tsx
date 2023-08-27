86427fefe8d8b6cc1b65df8910f039aa1feb3706
michaelcibekwe <michaelcibekwe@gmail.com>
2023-08-26 23:02:04 -0500

    style: established file format

---
+++ src/components/form/LoginForm.tsx
@@ -0,0 +1,16 @@
+// Login form
+// Contains:
+// Username/Email and Password
+
+// Tell user that it is invalid AFTER pushing the login button if it is not working.
+import React from "react"
+
+function LoginForm () {
+
+    return (
+        <h1>Login Form</h1>
+
+    )
+}
+
+export default LoginForm
---
+++ src/components/form/SignUpForm.tsx
@@ -0,0 +1,15 @@
+// Sign up:
+
+// Contains:
+// First and Last Name, Password, Email and confirms password.
+import React from "react"
+
+function SignUpForm () {
+
+    return (
+        <h1>Login</h1>
+
+    )
+}
+
+export default SignUpForm
